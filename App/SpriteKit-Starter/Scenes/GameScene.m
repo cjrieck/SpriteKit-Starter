8745fb3495e4f5148b5925e6e23a3857a606bae1
cjrieck <cjrieck123@gmail.com>
2014-11-14 14:45:10 -0500

    monsters scroll across the screen every second

--- App/SpriteKit-Starter/Scenes/GameScene.m
+++ App/SpriteKit-Starter/Scenes/GameScene.m
@@ -12,6 +12,9 @@
 
 @property (strong, nonatomic) SKSpriteNode *heroSprite;
 
+@property (assign, nonatomic) NSTimeInterval lastSpawnInterval;
+@property (assign, nonatomic) NSTimeInterval lastUpdateInterval;
+
 @end
 
 @implementation GameScene
@@ -20,8 +23,6 @@
 {
     self = [super initWithSize:size];
     if ( self ) {
-        NSLog(@"Size: %@", NSStringFromCGSize(size));
-        
         self.backgroundColor = [UIColor whiteColor];
         
         _heroSprite = [[SKSpriteNode alloc] initWithImageNamed:@"Hero"];
@@ -33,8 +34,49 @@
     return self;
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+- (void)addMonster
+{
+    SKSpriteNode *monsterNode = [SKSpriteNode spriteNodeWithImageNamed:@"Orange-Monster"];
+    monsterNode.xScale = 0.1;
+    monsterNode.yScale = 0.1;
+    
+    int minY = monsterNode.size.height / 2.0f;
+    int maxY = self.frame.size.height - minY;
+    int rangeY = maxY - minY;
+    int actualY = (arc4random() % rangeY) + minY;
+    
+    monsterNode.position = CGPointMake(self.frame.size.width + monsterNode.size.width / 2, actualY);
+    [self addChild:monsterNode];
+    
+    int minDuration = 2.0f;
+    int maxDuration = 4.0f;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    SKAction *moveAction = [SKAction moveTo:CGPointMake(-monsterNode.size.width/2, actualY) duration:actualDuration];
+    SKAction *moveDoneAction = [SKAction removeFromParent];
+    [monsterNode runAction:[SKAction sequence:@[moveAction, moveDoneAction]]];
+}
+
+- (void)updateWithTimeSinceLastUpdate:(CFTimeInterval)timeSinceLast
+{
+    self.lastSpawnInterval += timeSinceLast;
+    if ( self.lastSpawnInterval >= 1 ) {
+        self.lastSpawnInterval = 0.0;
+        [self addMonster];
+    }
+}
+
+-(void)update:(CFTimeInterval)currentTime
+{
+    CFTimeInterval timeSinceLast = currentTime - self.lastUpdateInterval;
+    self.lastUpdateInterval = currentTime;
+    if ( timeSinceLast >= 1 ) {
+        timeSinceLast = 1.0 / 60.0;
+        self.lastUpdateInterval = currentTime;
+    }
+    [self updateWithTimeSinceLastUpdate:timeSinceLast];
+    
 }
 
 @end
